1d5b2e81bea506347c61a8f6393e2f0414972fbf
Viny-Pereira <vinypereira75@gmail.com>
2023-11-06 20:28:18 -0300

    ajustes na interface

--- interface.py
+++ interface.py
@@ -1,10 +1,20 @@
 import tkinter as tk
 from dados_requisicao import DadosRequisicao
+import webbrowser
 
 class InterfaceUsuario:
     def __init__(self, root, dados_requisicao):
         self.root = root
         self.root.title("Obter Dados Hidrológicos")
+
+        # Defina o tamanho da janela e centralize-a na tela
+        largura_janela = 400
+        altura_janela = 300
+        largura_tela = root.winfo_screenwidth()
+        altura_tela = root.winfo_screenheight()
+        x = (largura_tela - largura_janela) // 2
+        y = (altura_tela - altura_janela) // 2
+        root.geometry(f"{largura_janela}x{altura_janela}+{x}+{y}")
 
         self.dados_requisicao = dados_requisicao
 
@@ -32,6 +42,16 @@ class InterfaceUsuario:
         self.mensagem_label = tk.Label(root, text="")
         self.mensagem_label.pack()
 
+        # Rótulo "Sobre"
+        self.sobre_label = tk.Label(root, text="Sobre | Feito por Viny Pereira.", font=("Arial", 8), fg="black")
+        self.sobre_label.pack(side="bottom", fill="x")
+        self.sobre_label.bind("<Button-1>", self.abrir_link_repositorio)
+
+        # Rótulo "Saiba mais"
+        self.saiba_mais_label = tk.Label(root, text="Saiba mais", font=("Arial", 8), fg="blue", cursor="hand2")
+        self.saiba_mais_label.pack(side="bottom", fill="x")
+        self.saiba_mais_label.bind("<Button-1>", self.abrir_link_repositorio)
+
     def obter_dados_e_salvar(self):
         cod_estacao = self.cod_estacao_entry.get()
         data_inicio = self.data_inicio_entry.get()
@@ -46,6 +66,9 @@ class InterfaceUsuario:
             mensagem = resultado
 
         self.mensagem_label.config(text=mensagem)
+
+    def abrir_link_repositorio(self, event):
+        webbrowser.open("https://github.com/Viny-Pereira/requisicao_pluvial_ana")
 
 if __name__ == "__main__":
     root = tk.Tk()
